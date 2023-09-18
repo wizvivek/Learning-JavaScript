702e5a7e21c83e9f56504b3d3b9d97d47a9ad062
vivek kumar <89367203+wizvivek@users.noreply.github.com>
2023-09-18 08:25:19 +0000

    03conversionOperations added

---
+++ o1basics/03conversionOperations.js
@@ -0,0 +1,32 @@
+let score=33;
+
+// console.log(typeof(score));
+
+score="33abc";
+// console.log(typeof(score));
+
+let intscore=Number(score);
+// console.log(typeof(intscore));
+// console.log(intscore);
+
+
+//-------- OPERATIONS
+
+// let value=3;
+// let negvalue=-value;
+// console.log(value);
+// console.log(negvalue);
+
+
+let str1="hello ";
+let str2="vivek";
+let str3=str1+str2;
+// console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+
